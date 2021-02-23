9a231e2ac3a55d277efdc9b1edeb400d7653abb1
Artist Krishna <rtistkrishna@gmail.com>
2021-02-23 23:22:05 +0530

    Version 1

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MoozyRestaurantApp());
+}
+
+class MoozyRestaurantApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
